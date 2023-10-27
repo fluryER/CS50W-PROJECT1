ae01d6570ec29020c0e8f4f18923ea2379562f15
fluryER <yamitalanzas@gmail.com>
2023-10-26 22:43:26 -0600

    SE TERMINO

--- aplication.py
+++ aplication.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask, render_template, request, flash, redirect, url_for,session
+from flask import Flask, render_template, request, flash, redirect, url_for,session,jsonify
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_session import Session
 import requests
 from functools import wraps
+
 
 def login_required(f):
     @wraps(f)
@@ -146,7 +147,9 @@ def libro_detalles(isbn):
         year = book_info.get("publishedDate", "Año de publicación no disponible.")
         rating = book_info.get("averageRating", 0)
         rating_count = book_info.get("ratingsCount", 0)
+        comentario = text("SELECT *FROM review WHERE isbn=:isbn")
         # return(f'{book_img}')
+        comment = db.execute(comentario,{"isbn":isbn}).fetchall()
         return render_template("detalles.html", 
                                isbn=isbn,
                                description=description,
@@ -156,10 +159,88 @@ def libro_detalles(isbn):
                                year=year,
                                rating=rating,
                                rating_count=rating_count,
+                               comments=comment
+                        
                                )
+    
     else:
         print("El api en estos momentos no esta disponible")
 
+@app.route('/reseña', methods=['POST'])
+def reseña():
+     user_id = session['user_id']
+     isbn = request.form.get('isbn')    
+     ranting = request.form.get("puntuacion")
+     comentario = request.form.get("comentario")
+     review_query = text("SELECT* FROM review WHERE user_id=:user_id and isbn=:isbn")
+     review = db.execute(review_query,{"user_id" :user_id, "isbn" :isbn}).fetchone()
+
+     if review:
+         return(f"Ya has enviado una reseña para este libro: {isbn}")
+     
+ 
+     if not isbn or not ranting or not comentario:
+         return ("No se ha podido crear la reseña, porfavor escribe una reseña nuevamente")
+
+     try:
+         
+         crear_review = text("INSERT INTO review(user_id, isbn, score, comment) VALUES(:user_id, :isbn, :ranting,:comentario)")
+         db.execute(crear_review,{"user_id":user_id, "isbn" :isbn, "ranting":ranting, "comentario":comentario})
+         db.commit()
+         db.close()
+         return redirect(f'/libro_detalles/{isbn}')
+     
+     except Exception as e:
+         db.rollback()
+         raise(e)
+         return(f"Error al crear comentario")
+     
+@app.route('/api/<string:isbn>')  
+
+def api(isbn):
+    api = text('''SELECT b.author, b.title, b.year, b.isbn, AVG(r.score) AS average_score, COUNT(r.id) AS review_count 
+         FROM books AS b 
+         LEFT JOIN review AS r ON b.isbn = r.isbn 
+         WHERE b.isbn = :isbn 
+         GROUP BY b.author, b.title, b.year, b.isbn''')
+    
+    resultado_api = db.execute(api, {"isbn":isbn}).fetchone()
+
+    if resultado_api is not None:
+        author = resultado_api[0]
+        title =  resultado_api[1]
+        year =  resultado_api[2]
+        isbn =  resultado_api[3]
+        review_count = resultado_api[4]
+
+        if resultado_api[4] is not None:
+            average_score = round(resultado_api[4], 2)
+
+        else:
+
+            average_score = None
+
+        resultado= {"title":title,
+                        
+                        "author":author,
+                        "year":year,
+                        "isbn": isbn,
+                        "review_count":review_count,
+                        "average_score":average_score }
+        
+        return jsonify(resultado)
+    else:
+        respuesta = {
+            "mensaje":f"El isbn:{isbn} no se encuentra en la base de datos"
+
+        }
+        return jsonify(respuesta)
+
+        
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True) #Hasta aqui ya conectamos las variables de entorno
+
+    
